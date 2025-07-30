6001dfc5621d374ee3a55ce016017a4775093b82
Aditya Bajaj <141822662+Bajaj041@users.noreply.github.com>
2025-07-30 05:03:36 +0000

    ans1

---
+++ lab1/ans1.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    printf("Enter the number of integers: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    printf("Enter %d integers:\n", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    int sum = 0;
+    for (int i = 0; i < n; i++) {
+        sum += arr[i];
+    }
+
+    double average = (double)sum / n;
+    printf("Average = %.2lf\n", average);
+
+    return 0;
+}
